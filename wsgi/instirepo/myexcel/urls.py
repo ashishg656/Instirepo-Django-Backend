4d4cafb024dabdc3c0baffc9546721e9eaee3742
ashish <ashishg656@gmail.com>
2016-01-04 00:08:50 +0530

    fiexd url problem

--- wsgi/instirepo/myexcel/urls.py
+++ wsgi/instirepo/myexcel/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^get_works_list', views.get_works_list, name='get_works_list'),
 
-    # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
+    url(r'^add_work', views.add_work, name='add_work'),
     # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
     # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
     # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
