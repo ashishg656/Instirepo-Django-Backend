2da333e0cba88351e6c797a98624fa96b505cf7a
ashish <ashishg656@gmail.com>
2016-01-04 13:58:55 +0530

    added delete work url

--- wsgi/instirepo/myexcel/urls.py
+++ wsgi/instirepo/myexcel/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     url(r'^add_detail', views.add_detail, name='add_detail'),
 
     url(r'^get_details_list', views.get_details_list, name='get_details_list'),
+
+    url(r'^delete_work', views.delete_work, name='delete_work'),
     
-    # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
     # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
     # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
     # url(r'^view_addresses', views.view_addresses, name='view_addresses'),
