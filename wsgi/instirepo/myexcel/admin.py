86866de25738c2126798d4a8330d009c1c59a377
ashish <ashishg656@gmail.com>
2016-01-06 15:23:51 +0530

    upgraded django to 1.9.1

--- wsgi/instirepo/myexcel/admin.py
+++ wsgi/instirepo/myexcel/admin.py
@@ -1,19 +1,30 @@
 from django.contrib import admin
+from django.forms import Textarea
 from myexcel.models import *
 
 
 class ChoiceInline(admin.TabularInline):
     model = Details
-    extra = 3
+    extra = 1
 
 
 class WorksAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('tool_number', 'jw_number', 'product', 'date', 'target_date', 'status', 'remarks')
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(
+            attrs={'rows': 4,
+                   'cols': 90, })},
+    }
 
 
 class DetailsAdmin(admin.ModelAdmin):
     list_display = ('date', 'trial_date', 'action_taken', 'status', 'remarks')
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(
+            attrs={'rows': 4,
+                   'cols': 90, })},
+    }
 
 
 admin.site.register(Works, WorksAdmin)
