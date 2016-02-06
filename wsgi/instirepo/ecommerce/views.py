8efbecb86ab3e6d63a25dfd9ed9f8379135951f3
ashish <ashishg656@gmail.com>
2016-02-06 17:42:13 +0530

    added view

--- wsgi/instirepo/ecommerce/views.py
+++ wsgi/instirepo/ecommerce/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import base64
+from ctypes import c_short
+from datetime import datetime, time
+from operator import itemgetter
+import os
 
-# Create your views here.
+import sys
+from django.core.files.base import ContentFile
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from PIL import Image
+from django.template import RequestContext
+from django.template.context_processors import request
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+from django.core import serializers
+import json
+import requests
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from instirepo import settings
+from ecommerce.models import *
+from django.core import serializers
+from push_notifications.models import GCMDevice
+from django.db.models import Q
+import pdb
+
+
+@csrf_exempt
+def get_all_product_categories_and_trending_and_recent_products(request):
+    user_id = request.POST.get('user_id')
+
+    user = User.objects.get(pk=int(user_id))
+    user_profile = user.user_profile.get()
+
+    categories = []
+
+    query = ProductCategories.objects.filter(is_active=True)
+    for cat in query:
+        image = None
+        try:
+            image = cat.image.url
+        except:
+            pass
+        categories.append({'id': cat.id, 'name': cat.name, 'image': image, 'type': cat.type, 'color': cat.color})
+
+    return JsonResponse({'categories': categories, 'error': error, 'message': message})
